54f5f20141ab95c6ec44d4f8c39518f60203b83b
Brian Fleming <bfleming@ciena.com>
2018-08-02 13:34:47 -0700

    Renable route table. It's required that you just pass in a list of empty strings if no routes are wanted.

--- vnet/main.tf
+++ vnet/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_subnet" "mysubnet" {
   virtual_network_name      = "${azurerm_virtual_network.vnet.name}"
   resource_group_name       = "${var.resource_group_name}"
   address_prefix            = "${var.subnet_prefixes[count.index]}"
-  # route_table_id            = "${var.route_tables[count.indexx]}"
+  route_table_id            = "${var.route_tables[count.indexx]}"
   # network_security_group_id = "${lookup(var.nsg_ids,var.subnet_names[count.index],"")}"
   # network_security_group_id = "${azurerm_network_security_group.ssh.id}"
   count                     = "${length(var.subnet_names)}"
